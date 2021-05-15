b6c7d79f05d6a8761e5f64a19fdff1fb117d29b3
Jordan Wright <jordanwright@Jordans-MacBook-Pro.local>
2021-05-15 14:50:08 -0500

    starts of ufo one

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,25 @@
 // from data.js
+// from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+console.log(data);
+
+
+
+// comment this out 
+
+
+
+
+
+data.forEach(function(ufoSighting){
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(function([key,value]){
+        console.log(key,value);
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
