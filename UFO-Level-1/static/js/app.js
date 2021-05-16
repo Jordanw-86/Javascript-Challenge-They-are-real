df2181e1879ffbb46b5f3bac7334af46774cc4fd
Jordan Wright <jordanwright@Jordans-MacBook-Pro.local>
2021-05-15 19:33:56 -0500

    edits to app.js

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,16 +1,10 @@
 // from data.js
-// from data.js
+
 var tableData = data;
 console.log(tableData);
 
 var tbody = d3.select("tbody");
 console.log(data);
-
-
-
-// comment this out 
-
-
 
 
 
@@ -21,5 +15,27 @@ data.forEach(function(ufoSighting){
         console.log(key,value);
         var cell = tbody.append("td");
         cell.text(value);
+    });
+});
+
+
+var button = d3.select("#filter-btn");
+
+button.on("click", function(event){
+    d3.event.preventDefault();
+    tbody.html("");
+
+var inputElement = d3.select("#datetime"); 
+var inputValue = inputElement.property("value");
+
+
+
+var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+filteredData.forEach(function(dateData){
+    var row=tbody.append("tr");
+    Object.entries(dateData).forEach(function([key,value]){
+    var cell=tbody.append("td");
+    cell.text(value);
+        });
     });
 });
