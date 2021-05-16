da046266b3a16a5e5b5d9d18f7e29d8a24a6ecf6
Jordan Wright <jordanwright@Jordans-MacBook-Pro.local>
2021-05-15 19:35:55 -0500

    ufo 2 start

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,18 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+console.log(data);
+
+
+data.forEach(function(ufoSighting){
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(function([key,value]){
+        console.log(key,value);
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
+
